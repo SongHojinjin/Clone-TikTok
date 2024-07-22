6be7a8ddb8963eba3a3a761f087366e4271d729f
songhj <songhojin913@gmail.com>
2024-07-22 22:27:17 +0900

    init

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TikTokApp extends StatelessWidget {
+  const TikTokApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    title: 'TikTok Clone',
+    theme: ThemeData(
+      primaryColor: const Color(0xFFE9435A),
+    ),
+    home: Container()
+    );
+  }
+}
