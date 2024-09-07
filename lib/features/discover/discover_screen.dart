ce3eb8e01d3c8b634cb90dcf11caa5ce8ed5b117
song hojin <songhojin913@gmail.com>
2024-09-07 21:13:32 +0900

    Discover 화면 크기에 따른 아이콘 여부
    LayoutBuilder

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -42,7 +42,7 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
+    final width = MediaQuery.of(context).size.width; // 화면의 크기가 바뀔 때마다 값이 바뀜
     return GestureDetector(
       onTap: () => FocusScope.of(context).unfocus(),
       child: DefaultTabController(
@@ -135,71 +135,75 @@ class _DiscoverScreenState extends State<DiscoverScreen> {
                 vertical: Sizes.size8,
               ),
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: width > Breakpoints.lg ? 5 : 2,
+                crossAxisCount: width > Breakpoints.md ? 5 : 2,
                 crossAxisSpacing: Sizes.size10,
                 mainAxisSpacing: Sizes.size10,
                 childAspectRatio: 9 / 21,
               ),
-              itemBuilder: (context, index) => Column(
-                children: [
-                  Container(
-                    clipBehavior: Clip.hardEdge,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(Sizes.size3),
+              itemBuilder: (context, index) => LayoutBuilder(
+                builder: (context, constraints) => Column(
+                  children: [
+                    Container(
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(Sizes.size3),
+                      ),
+                      child: AspectRatio(
+                        aspectRatio: 9 / 16,
+                        child: FadeInImage.assetNetwork(
+                            fit: BoxFit.cover,
+                            placeholder: 'assets/images/surprised.jpeg',
+                            image: 'https://img-store.theqoo.net/ghjUb.jpg'),
+                      ),
                     ),
-                    child: AspectRatio(
-                      aspectRatio: 9 / 16,
-                      child: FadeInImage.assetNetwork(
-                          fit: BoxFit.cover,
-                          placeholder: 'assets/images/surprised.jpeg',
-                          image: 'https://img-store.theqoo.net/ghjUb.jpg'),
+                    Gaps.v4,
+                    Text(
+                      '${constraints.maxWidth} Hello, You faced the lucky boy wearing red T-shirt. Hope you have a nice day. :D',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: Sizes.size16,
+                      ),
                     ),
-                  ),
-                  Gaps.v4,
-                  const Text(
-                    'Hello, You faced the lucky boy wearing red T-shirt. Hope you have a nice day. :D',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: Sizes.size16,
-                    ),
-                  ),
-                  Gaps.v4,
-                  DefaultTextStyle(
-                    style: TextStyle(
-                      color: Colors.grey.shade500,
-                      fontWeight: FontWeight.w500,
-                    ),
-                    child: Row(
-                      children: [
-                        const CircleAvatar(
-                          radius: Sizes.size16,
-                          backgroundImage: NetworkImage(
-                            'https://avatars.githubusercontent.com/u/74577721?v=4',
-                          ),
-                        ),
-                        Gaps.h6,
-                        const Expanded(
-                          child: Text(
-                            'Pizza_love_also_chicken',
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-                        Gaps.h4,
-                        FaIcon(
-                          FontAwesomeIcons.heart,
-                          size: Sizes.size14,
+                    Gaps.v4,
+                    if (constraints.maxWidth < 143 ||
+                        constraints.maxWidth > 200)
+                      DefaultTextStyle(
+                        style: TextStyle(
                           color: Colors.grey.shade500,
+                          fontWeight: FontWeight.w500,
                         ),
-                        Gaps.h3,
-                        const Text('9.9M'),
-                        Gaps.h8,
-                      ],
-                    ),
-                  )
-                ],
+                        child: Row(
+                          children: [
+                            const CircleAvatar(
+                              radius: Sizes.size16,
+                              backgroundImage: NetworkImage(
+                                'https://avatars.githubusercontent.com/u/74577721?v=4',
+                              ),
+                            ),
+                            Gaps.h6,
+                            const Expanded(
+                              child: Text(
+                                'Pizza_love_also_chicken',
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ),
+                            Gaps.h4,
+                            FaIcon(
+                              FontAwesomeIcons.heart,
+                              size: Sizes.size14,
+                              color: Colors.grey.shade500,
+                            ),
+                            Gaps.h3,
+                            const Text('9.9M'),
+                            Gaps.h8,
+                          ],
+                        ),
+                      )
+                  ],
+                ),
               ),
             ),
             for (var tab in tabs.skip(1))
